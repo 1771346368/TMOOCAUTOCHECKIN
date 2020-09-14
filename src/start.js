73dfcbc35c63fa3c0c66a25cfcf38a4081b6359a
ghost <1771346368@qq.com>
2020-09-14 15:44:24 +0800

    无修改

--- src/start.js
+++ src/start.js
@@ -1,5 +1,7 @@
 window.onload = function () {
+  // window.document.onreadystatechange = function () {
   'use strict';
+  // if (window.document.readyState === "interactive") {
   chrome.storage.local.get('user', (result) => {
     chrome.storage.local.get('on', (res) => {
       // console.log(res.on, '***************')
@@ -84,7 +86,6 @@ window.onload = function () {
               // console.log('5555555555555')
               // console.log(bbb1)
               bbb1.click();
-              
               setTimeout(() => {
                 chrome.storage.local.set({ on: false }, () => { })
               }, 200);
@@ -101,6 +102,8 @@ window.onload = function () {
       }
     })
   })
+  // }
+}
   // chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   //   username = request.userName;
   //   password = request.passWord;
@@ -114,51 +117,51 @@ window.onload = function () {
   //   console.log(re)
   //   console.log('------------------')
   // }
-  /*
-  if (window.location.href === "http://www.tmooc.cn/") {
-    setInterval(() => {
-      document.querySelector('#tobbar_username').click();
-    }, 500);
-  } else if (window.location.href === "http://uc.tmooc.cn/login/jumpTologin") {
-    // var getInput = function () {
-    //   if (document.querySelector('#js_submit_login') == null) {
-    //     setTimeout(getInput, 100);
-    //   } else {
-    //     document.querySelector('#js_account_pm').value = '1771346368@qq.com'
-    //     document.querySelector('#js_password').value = 'dr123456'
-    //     document.querySelector('#js_submit_login').click();
-    //   }
-    // }
-    // getInput()
-    setTimeout(() => {
-      var inputClick = function () {
-        document.querySelector('#js_account_pm').value = '1771346368@qq.com'
-        document.querySelector('#js_password').value = 'dr123456'
-        document.querySelector('#js_submit_login').click();
-      }
-    }, 1000);
-  } else if (window.location.href === "http://uc.tmooc.cn/userCenter/toUserSingUpCoursePage" || window.location.href === 'http://uc.tmooc.cn/userCenter/toUserCenterPage') {
-    // var getCou = function () {
-    //   if (document.querySelector('.btn-0523x') == null) {
-    //     setTimeout(getCou, 100);
-    //   } else {
-    //     document.querySelector('.btn-0523x').click();
-    //   }
-    // }
-    setTimeout(() => {
-      document.querySelector('.btn-0523x').click();
-    }, 1000);
-  } else if (window.location.href === "http://tts.tmooc.cn/studentCenter/toMyttsPage") {
-    // var getCheckin = function () {
-    //   if (document.querySelector('.bbb1') == null) {
-    //     setTimeout(getCheckin, 100);
-    //   } else {
-    //     document.querySelector('.bbb1').click();
-    //   }
-    // }
-    setTimeout(() => {
-      document.querySelector('.bbb1').click();
-    }, 1000);
-  }
-*/
+/*
+if (window.location.href === "http://www.tmooc.cn/") {
+  setInterval(() => {
+    document.querySelector('#tobbar_username').click();
+  }, 500);
+} else if (window.location.href === "http://uc.tmooc.cn/login/jumpTologin") {
+  // var getInput = function () {
+  //   if (document.querySelector('#js_submit_login') == null) {
+  //     setTimeout(getInput, 100);
+  //   } else {
+  //     document.querySelector('#js_account_pm').value = '1771346368@qq.com'
+  //     document.querySelector('#js_password').value = 'dr123456'
+  //     document.querySelector('#js_submit_login').click();
+  //   }
+  // }
+  // getInput()
+  setTimeout(() => {
+    var inputClick = function () {
+      document.querySelector('#js_account_pm').value = '1771346368@qq.com'
+      document.querySelector('#js_password').value = 'dr123456'
+      document.querySelector('#js_submit_login').click();
+    }
+  }, 1000);
+} else if (window.location.href === "http://uc.tmooc.cn/userCenter/toUserSingUpCoursePage" || window.location.href === 'http://uc.tmooc.cn/userCenter/toUserCenterPage') {
+  // var getCou = function () {
+  //   if (document.querySelector('.btn-0523x') == null) {
+  //     setTimeout(getCou, 100);
+  //   } else {
+  //     document.querySelector('.btn-0523x').click();
+  //   }
+  // }
+  setTimeout(() => {
+    document.querySelector('.btn-0523x').click();
+  }, 1000);
+} else if (window.location.href === "http://tts.tmooc.cn/studentCenter/toMyttsPage") {
+  // var getCheckin = function () {
+  //   if (document.querySelector('.bbb1') == null) {
+  //     setTimeout(getCheckin, 100);
+  //   } else {
+  //     document.querySelector('.bbb1').click();
+  //   }
+  // }
+  setTimeout(() => {
+    document.querySelector('.bbb1').click();
+  }, 1000);
 }
+*/
+// }
